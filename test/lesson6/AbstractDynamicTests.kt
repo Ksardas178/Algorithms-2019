24594d2994c4ee7c391275c09ba1a326f414d16b
Сергей <ser154nexus@gmail.com>
2019-12-13 01:20:26 +0300

    Not working. Just updating..

--- test/lesson6/AbstractDynamicTests.kt
+++ test/lesson6/AbstractDynamicTests.kt
@@ -39,7 +39,7 @@ abstract class AbstractDynamicTests {
     }
 
     fun longestIncreasingSubSequence(longestIncreasingSubSequence: (List<Int>) -> List<Int>) {
-        /*assertEquals(listOf(), longestIncreasingSubSequence(listOf()))
+        assertEquals(listOf(), longestIncreasingSubSequence(listOf()))
         assertEquals(listOf(1), longestIncreasingSubSequence(listOf(1)))
         assertEquals(listOf(1, 2), longestIncreasingSubSequence(listOf(1, 2)))
         assertEquals(listOf(2), longestIncreasingSubSequence(listOf(2, 1)))
@@ -49,7 +49,7 @@ abstract class AbstractDynamicTests {
             longestIncreasingSubSequence(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
         )
         assertEquals(listOf(2, 12), longestIncreasingSubSequence(listOf(2, 12, 6)))
-        assertEquals(listOf(2, 8, 9, 12), longestIncreasingSubSequence(listOf(2, 8, 5, 9, 12, 6)))*/
+        assertEquals(listOf(2, 8, 9, 12), longestIncreasingSubSequence(listOf(2, 8, 5, 9, 12, 6)))
         assertEquals(listOf(2, 8, 9, 12), longestIncreasingSubSequence(listOf(10, 2, 8, 5, 9, 12)))
         assertEquals(
             listOf(23, 34, 56, 87, 91, 98, 140, 349), longestIncreasingSubSequence(
